21928d3a3103e4f320dc8c9968acc5d47bad8a33
koichi.ikeda <koichiikeda.0111@gmail.com>
2025-11-23 18:33:56 +0900

    fix: set cloudflare access include emails

--- infra/terraform/modules/cloudflare_access/main.tf
+++ infra/terraform/modules/cloudflare_access/main.tf
@@ -12,16 +12,24 @@ variable "domain" {
 
 variable "allowed_emails" {
   type = list(string)
+
+  validation {
+    condition     = length(var.allowed_emails) > 0
+    error_message = "At least one email address must be provided for Access."
+  }
 }
 
 resource "cloudflare_zero_trust_access_policy" "allow_emails" {
-  account_id     = var.account_id
-  name           = "allow-devs"
-  decision       = "allow"
-  approval_groups = [{
-    approvals_needed = 1
-    email_addresses = var.allowed_emails
-  }]
+  account_id = var.account_id
+  name       = "allow-devs"
+  decision   = "allow"
+  include = [
+    for email in var.allowed_emails : {
+      email = {
+        email = email
+      }
+    }
+  ]
 }
 
 resource "cloudflare_zero_trust_access_application" "this" {
