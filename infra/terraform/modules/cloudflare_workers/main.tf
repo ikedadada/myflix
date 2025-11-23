a087f3abe6698e814397472545c46e7c62434733
koichi.ikeda <koichiikeda.0111@gmail.com>
2025-11-23 15:18:50 +0900

    fix: add account id to workers script

--- infra/terraform/modules/cloudflare_workers/main.tf
+++ infra/terraform/modules/cloudflare_workers/main.tf
@@ -19,7 +19,8 @@ variable "r2_binding_name" {
 }
 
 resource "cloudflare_workers_script" "this" {
-  name    = var.script_name
+  account_id = var.account_id
+  name       = var.script_name
   content = <<EOT
 export default {
   async fetch() {
