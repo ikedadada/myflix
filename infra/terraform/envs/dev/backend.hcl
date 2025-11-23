d4ed751022fd3e96a9b7df8b67b45eb2d483e364
koichi.ikeda <koichiikeda.0111@gmail.com>
2025-11-23 17:13:52 +0900

    chore: move terraform backend to backend.hcl

---
+++ infra/terraform/envs/dev/backend.hcl
@@ -0,0 +1,8 @@
+bucket                      = "myflix-dev-state"
+key                         = "terraform/dev/terraform.tfstate"
+region                      = "auto"
+endpoint                    = "https://<your-account-id>.r2.cloudflarestorage.com"
+skip_credentials_validation = true
+skip_region_validation      = true
+skip_metadata_api_check     = true
+force_path_style            = true
--- infra/terraform/envs/dev/main.tf
+++ infra/terraform/envs/dev/main.tf
@@ -6,16 +6,6 @@ terraform {
       version = "5.13.0"
     }
   }
-  backend "s3" {
-    bucket                      = "myflix-dev-state"
-    key                         = "terraform/dev/terraform.tfstate"
-    region                      = "auto"
-    endpoint                    = "https://<your-account-id>.r2.cloudflarestorage.com"
-    skip_credentials_validation = true
-    skip_region_validation      = true
-    skip_metadata_api_check     = true
-    force_path_style            = true
-  }
 }
 
 provider "cloudflare" {
