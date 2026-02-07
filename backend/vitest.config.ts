219e30efe913d74c686c02cba336b65d6cd523ca
koichi.ikeda <koichiikeda.0111@gmail.com>
2026-02-07 10:38:57 +0900

    fix:vitest.config.ts上で出ていたエラーの修正

--- backend/vitest.config.ts
+++ backend/vitest.config.ts
@@ -1,13 +1,10 @@
-import { dirname, resolve } from 'node:path'
-import { fileURLToPath } from 'node:url'
+import * as path from 'node:path'
 import { defineConfig } from 'vitest/config'
-
-const __dirname = dirname(fileURLToPath(import.meta.url))
 
 export default defineConfig({
   resolve: {
     alias: {
-      '@': resolve(__dirname, 'src'),
+      '@': path.resolve(__dirname, 'src'),
     },
   },
   test: {
